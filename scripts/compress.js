c81ad2021d6e5d2c10d6bca5487627469420230d
prowebdev110 <tieosong119@gmail.com>
2022-12-27 01:37:02 +0900

    Update compress.js

--- scripts/compress.js
+++ scripts/compress.js
@@ -10,8 +10,6 @@ dirs.forEach((dir) => {
     console.log(dir)
     if (file.endsWith(".xml")) {
       // gzip
-      const fileContents = fs.createReadStream(dir + "/" + file);
-      const writeStream = fs.createWriteStream(dir + "/" + file + ".gz");
       const zip = zlib.createGzip();
 
       fileContents
