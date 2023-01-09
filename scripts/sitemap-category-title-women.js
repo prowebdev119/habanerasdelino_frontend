e010f7d87e7105b21cf076ddf0e34b40b9bde556
prowebdev110 <tieosong119@gmail.com>
2023-01-09 14:44:22 +0900

    Update sitemap-category-title-women.js

--- scripts/sitemap-category-title-women.js
+++ scripts/sitemap-category-title-women.js
@@ -30,6 +30,14 @@ const formatted = sitemap => prettier.format(sitemap, { parser: "html" });
   `;
 
   const generatedSitemap = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+    >
+      ${postListSitemap}
+    </urlset>
   `;
 
   const formattedSitemap = formatted(generatedSitemap);
